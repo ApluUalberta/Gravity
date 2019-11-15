53a901cef8bd584f79b3222313b470382abe2524
ApluUalberta <aplu@ualberta.ca>
2019-11-14 17:04:49 -0700

    "initial commit. Line Chart test fails"

--- app/src/main/java/com/example/gravity/MainActivity.java
+++ app/src/main/java/com/example/gravity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gravity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this,UserProfileActivity.class);
+        startActivity(intent);
 
     }
 }
