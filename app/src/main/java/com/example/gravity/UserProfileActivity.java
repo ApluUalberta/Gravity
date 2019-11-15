3d09cb98984d6ca0917503e4398dd9f27f29b559
ApluUalberta <aplu@ualberta.ca>
2019-11-14 17:42:48 -0700

    "Line Chart Stuff made"

--- app/src/main/java/com/example/gravity/UserProfileActivity.java
+++ app/src/main/java/com/example/gravity/UserProfileActivity.java
@@ -1,54 +1,39 @@
 package com.example.gravity;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
-import java.util.List;
-
-import lecho.lib.hellocharts.model.Axis;
-import lecho.lib.hellocharts.model.AxisValue;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.view.LineChartView;
-
-public class UserProfileActivity extends AppCompatActivity{
-
-    LineChartView progressChartView;
-    // temporary Data. Will be initialized with rep max calculator values.
-    String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
-            "Oct", "Nov", "Dec"};
-    int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
+public class UserProfileActivity extends AppCompatActivity {
+    LineChart lineChart;
+    LineData lineData;
+    LineDataSet lineDataSet;
+    ArrayList lineEntries;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // firebase
-        LineChartView progressChartView = findViewById(R.id.progressChart);
-        List yAxisValues = new ArrayList();
-        List axisValues = new ArrayList();
-        Line line = new Line(yAxisValues);
-
-
-        for(int i = 0; i < axisData.length; i++){
-            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
-        }
-
-        for (int i = 0; i < yAxisData.length; i++){
-            yAxisValues.add(new PointValue(i, yAxisData[i]));
-        }
-
-        List lines = new ArrayList();
-        lines.add(line);
-
-        LineChartData data = new LineChartData();
-        data.setLines(lines);
-
-        progressChartView.setLineChartData(data);
-
-        Axis axis = new Axis();
-        axis.setValues(axisValues);
-        data.setAxisXBottom(axis);
+        setContentView(R.layout.activity_userprofileactivity);
+        lineChart = findViewById(R.id.lineChart);
+        getEntries();
+        lineDataSet = new LineDataSet(lineEntries, "");
+        lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(18f);
+    }
+    private void getEntries() {
+        lineEntries = new ArrayList<>();
+        lineEntries.add(new Entry(2f, 0));
+        lineEntries.add(new Entry(4f, 1));
+        lineEntries.add(new Entry(6f, 1));
+        lineEntries.add(new Entry(8f, 3));
+        lineEntries.add(new Entry(7f, 4));
+        lineEntries.add(new Entry(3f, 3));
     }
 }
